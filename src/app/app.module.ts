29d9c3f317233eeaa85443f2910c572c7f8492ed
musloda <musloda@yahoo.com>
2018-10-04 17:52:27 +0200

    deuxieme commit galerie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { GalerieComponent } from './galerie/galerie.component';
+import { MediaModalComponent } from './media-modal/media-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalerieComponent,
+    MediaModalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
