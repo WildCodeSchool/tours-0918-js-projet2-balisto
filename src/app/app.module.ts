d87456a3ab7db850df5d039c43128589e89890f8
musloda <musloda@yahoo.com>
2018-10-08 22:21:34 +0200

    ajout pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { AppComponent } from './app.component';
 import { GalerieComponent } from './galerie/galerie.component';
+import { PageProduitsComponent } from './page-produits/page-produits.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     GalerieComponent,
+    PageProduitsComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
-    FormsModule
+    FormsModule,
+    NgxPaginationModule
 
   ],
   providers: [],
---
+++ src/app/page-produits/page-produits.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-produits',
+  templateUrl: './page-produits.component.html',
+  styleUrls: ['./page-produits.component.css']
+})
+export class PageProduitsComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+}
