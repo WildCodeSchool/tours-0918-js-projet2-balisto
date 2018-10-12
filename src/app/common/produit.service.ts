78aee628101d76ffef22fd212cef59d2c2869075
musloda <mus_tapha79@outlook.com>
2018-10-12 11:02:32 +0200

    push du service

---
+++ src/app/common/produit.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Produit } from './produit';
+import products from './tableau_produits';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProduitService {
+// déclaration du tableau produits de type Todo
+ tab: Produit[];
+
+  constructor() {
+// Si la clé n'éxiste "produits" pas dans le local storage
+     if (!localStorage.tab) {
+// Initialisation du local storage et du tableau produits
+    this.tab = products.map((x) => {
+      const produit = new Produit();
+      produit.id = x['_id'];
+      produit.name = x['product_name'];
+      produit.image = x['images'];
+      return produit;
+ });
+    this.saveToLocalStorage([this.tab]);
+     } else {
+// Si la clé "produits" existe récupération des donnée en conversion
+// en objet javascript (json)
+      const data = JSON.parse(localStorage.tab);
+// converte data to Produit model
+      this.tab = data;
+     }
+  }
+
+saveToLocalStorage(produit) {
+    const data = JSON.stringify(produit);
+    localStorage.setItem('produits', data);
+  }
+// retourne le tableau des produits
+get(): Produit[] {
+  return this.tab;
+  }
+
+// iteration sur tableau avec id
+
+}
---
+++ src/app/common/produit.ts
@@ -0,0 +1,5 @@
+export class Produit {
+  id: string | number;
+  name: string;
+  image: any;
+}
