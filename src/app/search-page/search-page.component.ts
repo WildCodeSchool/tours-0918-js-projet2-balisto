fbc2421900758f0172d1a036238c29a16159dcf3
Johann Teyssandier <johann.teyssandier@protonmail.com>
2018-10-12 10:05:05 +0200

    tentativs

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
-import products from '/home/stark/Bureau/Projet_2/tours-0918-js-projet2-balisto/src/products.ts';
+import products from '/home/stark/Bureau/Projet_2/tours-0918-js-projet2-balisto/src/products';
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
@@ -13,8 +13,19 @@ export class SearchPageComponent implements OnInit {
    }
   resultat: string;
   search = this.route.snapshot.params['search'];
+  products: any;
+  prodTri: any[];
+  product_name: any;
 
   ngOnInit() {
     console.log(this.route.snapshot.params['search']);
+    this.products = products;
   }
-}
+  affich(search) {
+    this.prodTri = {{products_name}}.filter(product => {
+      if (product.name && product.name.toLowerCase().split(',').includes(category.toLowerCase())) {
+        return product;
+      }
+    });
+  }
+
