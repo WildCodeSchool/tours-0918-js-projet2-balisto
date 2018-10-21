fc648a4706fbf9f522de7896c4a7b82e9d7cf893
Killian <kiloups@gmail.com>
2018-10-21 23:54:59 +0200

    ajout fonction supprimé

--- src/app/pagetri/pagetri.component.ts
+++ src/app/pagetri/pagetri.component.ts
@@ -14,15 +14,36 @@ export class PagetriComponent implements OnInit {
   tabTri: Produit[];
   page = 1;
   title: string;
+  produit: Produit;
+  produits: Produit[];
 
   constructor(private produitservice: ProduitService, public loginService: LoginService) { }
 
 
   ngOnInit() {
     this.tab = this.produitservice.get();
+    this.produits = this.getFromLocalStorage();
   }
 
   triByCategory(categories) {
     this.tabTri = this.produitservice.triByCategory(categories);
   }
+
+  deleteProduit(produit: Produit) {
+    const index = this.tab.findIndex( x => x.id === produit.id);
+    this.tab.splice(index, 1);
+  }
+
+  deleteProduittri(produit: Produit) {
+    const indextri = this.tabTri.findIndex( x => x.id === produit.id);
+    this.tabTri.splice(indextri, 1);
+  }
+
+  getFromLocalStorage(): Produit[] {
+    const stringData = localStorage.getItem('produit');
+    const produits: Produit[] = JSON.parse(stringData);
+
+    return produits;
+  }
+
 }
