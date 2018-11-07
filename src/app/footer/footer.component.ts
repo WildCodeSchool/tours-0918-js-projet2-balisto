e37fd7dbe5817dd719a82ae2039a88061ec07d40
Killian <kiloups@gmail.com>
2018-11-07 12:01:53 +0100

    getdismiss reason suppr

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -30,19 +30,7 @@ export class FooterComponent implements OnInit {
   open(content) {
     this.modalService.open(content).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
   }
 // Envoi le message ecris sur contact us dans un tableau "Media"
   envoi() {
